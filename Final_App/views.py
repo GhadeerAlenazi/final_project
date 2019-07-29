b15996fa66584961a7bcf2ea7f1c5a302a8d8f68
GhadeerAlenazi <ghadeer.m978@gmail.com>
2019-07-29 14:53:17 +0300

    add html file

--- Final_App/views.py
+++ Final_App/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# from .forms import 
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+# from .models import
 
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')   
 # Create your views here.
